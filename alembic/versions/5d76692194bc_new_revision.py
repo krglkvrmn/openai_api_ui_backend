61fc09518deb2c498eebf47701c65fd85c7a4d36
krglkvrmn <kruglikov1911@mail.ru>
2024-02-26 03:57:52 +0400

    Fix migration conflicts

--- alembic/versions/5d76692194bc_new_revision.py
+++ alembic/versions/5d76692194bc_new_revision.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '5d76692194bc'
-down_revision: Union[str, None] = 'a2d7ee05e3e3'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
