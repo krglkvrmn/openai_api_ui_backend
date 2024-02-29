084173da5635191871d78c12d8564bef61a14230
krglkvrmn <kruglikov1911@mail.ru>
2024-03-01 03:46:25 +0400

    Remove public port

--- app/core/config.py
+++ app/core/config.py
@@ -85,10 +85,9 @@ APP_LOCAL_HOST = os.getenv("APP_LOCAL_HOST", default='localhost')
 APP_LOCAL_PORT = os.getenv("APP_LOCAL_PORT", default=8000)
 
 APP_PUBLIC_HOST = os.getenv("APP_PUBLIC_HOST")
-APP_PUBLIC_PORT = 433
 
 if ENV_TYPE == 'PROD':
-    APP_ORIGIN = f'https://{APP_PUBLIC_HOST}:{APP_PUBLIC_PORT}'
+    APP_ORIGIN = f'https://{APP_PUBLIC_HOST}'
 elif ENV_TYPE == 'DEV':
     APP_ORIGIN = f'http://{APP_LOCAL_HOST}:{APP_LOCAL_PORT}'
 else:
