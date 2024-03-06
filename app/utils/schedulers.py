e3e98bd15dc17b647a4e138d6c2f89e54607613c
krglkvrmn <kruglikov1911@mail.ru>
2024-03-07 00:30:21 +0400

    True async scheduling

--- app/utils/schedulers.py
+++ app/utils/schedulers.py
@@ -1,16 +1,13 @@
 import asyncio
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from app.auth.database import delete_expired_guests, delete_expired_unverified_users
 
-event_loop = asyncio.get_event_loop()
 
-guests_deleter_scheduler = BackgroundScheduler()
-guests_deleter_scheduler.add_job(lambda: event_loop.create_task(delete_expired_guests()), 'interval', seconds=30)
+guests_deleter_scheduler = AsyncIOScheduler()
+guests_deleter_scheduler.add_job(delete_expired_guests, 'interval', seconds=30)
 
 
-unverified_deleter_scheduler = BackgroundScheduler()
-unverified_deleter_scheduler.add_job(
-    lambda: event_loop.create_task(delete_expired_unverified_users()), 'interval', seconds=60
-)
+unverified_deleter_scheduler = AsyncIOScheduler()
+unverified_deleter_scheduler.add_job(delete_expired_unverified_users, 'interval', seconds=60)
