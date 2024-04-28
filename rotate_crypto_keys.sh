f09ff0195274bbd3f1fc813499c4a13a37fd3236
krglkvrmn <kruglikov1911@mail.ru>
2024-04-28 15:35:24 +0400

    Update rotation script

--- rotate_crypto_keys.sh
+++ rotate_crypto_keys.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "You should provide exactly one argument - path to secrets directory"
+    exit 1
+fi
+
 openssl rand -base64 512 > $1/access_token_secret
 openssl rand -base64 512 > $1/refresh_token_secret
 openssl rand -base64 512 > $1/reset_password_token_secret
 openssl rand -base64 512 > $1/verification_token_secret
 # Be careful rotating this key, this will make all API keys saved in a database inaccessible
-openssl rand -base64 512 > $1/key_encode_key
+echo "Do you want to rotate 'key_encode_key'? This will make all API keys stored in a database inaccessible"
+read -p "(y/n) > " answer
+if [[ $answer = [Yy]* ]]; then
+    openssl rand -base64 512 > $1/key_encode_key
+fi
